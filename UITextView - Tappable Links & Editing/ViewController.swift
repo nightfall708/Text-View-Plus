c750f6d55dd2369db8f307f490301d05f88da6d7
Tal Shrestha <talshrestha@gmail.com>
2017-03-23 21:33:34 +0200

    All files

---
+++ UITextView - Tappable Links & Editing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UITextView - Tappable Links & Editing
+//
+//  Created by Tal Shrestha on 23/03/2017.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.textView.attributedText = NSAttributedString(string: "http://www.google.com", attributes: [NSLinkAttributeName: NSURL(string:"http://www.google.com")!])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
